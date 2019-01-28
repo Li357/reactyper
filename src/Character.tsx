d01fb544708c928be5be49e3ecfc349604044cf7
Andrew Li <andrewli357@gmail.com>
2019-01-27 20:14:41 -0600

    Rename Char to Character

--- src/Character.tsx
+++ src/Character.tsx
@@ -3,6 +3,6 @@ import * as React from 'react';
 import { ICharacterProps } from './types';
 import './styles/Character.css';
 
-export default function Char({ value, status }: ICharacterProps) {
+export default function Character({ value, status }: ICharacterProps) {
   return (<span className={`react-typer-char ${status}`}>{value}</span>);
 }

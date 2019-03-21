1e6064539423be325d7a8a13cc3685c6adb19286
Andrew Li <andrewli357@gmail.com>
2019-03-21 12:18:56 -0500

    Rewrite tests to fit new Typer system, finished up event handler testing

--- tests/unit/Typer.spec.tsx
+++ tests/unit/Typer.spec.tsx
@@ -3,20 +3,25 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Typer from '../../src/Typer';
-import { TyperState, ITyperProps, ITyperState } from '../../src/types';
+import { TyperState, ITyperProps } from '../../src/types';
 import { testInstance } from './utils';
 
 configure({ adapter: new Adapter() });
 jest.useFakeTimers();
 
-describe('Typer', () => {
-  const preTypeDelay = 1;
-  const typeDelay = 1;
-  const preEraseDelay = 1;
-  const eraseDelay = 1;
+const waitForAsync = () => new Promise((resolve) => setImmediate(resolve));
+const advanceTimersByTime = async (...times: number[]) => {
+  for (const time of times) {
+    jest.advanceTimersByTime(time);
+    await waitForAsync(); // See https://github.com/airbnb/enzyme/issues/1587
+  }
+};
 
-  const typeTimeout = preTypeDelay + typeDelay;
-  const eraseTimeout = preEraseDelay + eraseDelay;
+describe('Typer', () => {
+  const preTypeDelay = 1; // Delay before first type step, step happens directly after - IDLE during
+  const typeDelay = 1; // Delay for each successive time step - TYPING
+  const preEraseDelay = 1; // Delay before first erase step - IDLE
+  const eraseDelay = 1; // Delay for each successive erase step - ERASING
 
   const renderWithProps = (props: Partial<ITyperProps>) => shallow<Typer>(<Typer {...props} />);
 
@@ -37,11 +42,11 @@ describe('Typer', () => {
       describe(`Handle ${emojiLength} codepoint emojis`, () => {
         const emojiList = testEmojis[emojiLength];
         emojiList.forEach((emoji: string) => {
-          it(`should handle ${emoji} as one character`, () => {
+          it(`should handle ${emoji} as one character`, async () => {
             const instance = renderWithProps({ spool: [emoji], preTypeDelay, typeDelay });
             const expectState = testInstance(instance);
 
-            jest.advanceTimersByTime(typeTimeout);
+            await advanceTimersByTime(preTypeDelay);
             expectState({ wordIndex: 1 });
           });
         });
@@ -62,65 +67,140 @@ describe('Typer', () => {
       })
     );
 
-    it('should not repeat and should not erase on completion', () => {
+    it('should not repeat and should not erase on completion', async () => {
       const instance = renderWithOptions(0, false);
       const expectState = testInstance(instance);
 
-      jest.advanceTimersByTime(typeTimeout);
+      await advanceTimersByTime(preTypeDelay);
       expectState({ typerState: TyperState.COMPLETE, repeatCount: 0 });
     });
 
-    it('should not repeat and should erase on completion', () => {
+    it('should not repeat and should erase on completion', async () => {
       const instance = renderWithOptions(0, true);
       const expectState = testInstance(instance);
 
-      jest.advanceTimersByTime(typeTimeout);
-      expectState({ typerState: TyperState.ERASING, repeatCount: 0 });
-      jest.advanceTimersByTime(eraseTimeout);
+      await advanceTimersByTime(preTypeDelay);
+      expectState({ typerState: TyperState.IDLE, repeatCount: 0 });
+
+      await advanceTimersByTime(preEraseDelay);
       expectState({ typerState: TyperState.COMPLETE, repeatCount: 0 });
     });
 
-    it('should repeat specified times and should not erase on completion', () => {
+    it('should repeat specified times and should not erase on completion', async () => {
       const instance = renderWithOptions(1, false);
       const expectState = testInstance(instance);
 
-      jest.advanceTimersByTime(typeTimeout);
-      expectState({ typerState: TyperState.ERASING, repeatCount: 0 });
-      jest.advanceTimersByTime(eraseTimeout);
-      expectState({ typerState: TyperState.TYPING, repeatCount: 1 });
-      jest.advanceTimersByTime(typeTimeout);
+      await advanceTimersByTime(preTypeDelay);
+      expectState({ typerState: TyperState.IDLE, repeatCount: 0 });
+
+      await advanceTimersByTime(preEraseDelay);
+      expectState({ typerState: TyperState.IDLE, repeatCount: 0 });
+
+      await advanceTimersByTime(preTypeDelay); // repeatCount should increment at next type start
       expectState({ typerState: TyperState.COMPLETE, repeatCount: 1 });
     });
 
-    it('should repeat specified times and should erase on completion', () => {
+    it('should repeat specified times and should erase on completion', async () => {
       const instance = renderWithOptions(1, true);
       const expectState = testInstance(instance);
 
-      jest.advanceTimersByTime(typeTimeout);
-      expectState({ typerState: TyperState.ERASING, repeatCount: 0 });
-      jest.advanceTimersByTime(eraseTimeout);
-      expectState({ typerState: TyperState.TYPING, repeatCount: 1 });
+      await advanceTimersByTime(preTypeDelay);
+      expectState({ typerState: TyperState.IDLE, repeatCount: 0 });
+      await advanceTimersByTime(preEraseDelay);
+      expectState({ typerState: TyperState.IDLE, repeatCount: 0 });
 
-      jest.advanceTimersByTime(typeTimeout);
-      expectState({ typerState: TyperState.ERASING, repeatCount: 1 });
-      jest.advanceTimersByTime(eraseTimeout);
+      await advanceTimersByTime(preTypeDelay);
+      expectState({ typerState: TyperState.IDLE, repeatCount: 1 });
+      await advanceTimersByTime(preEraseDelay);
       expectState({ typerState: TyperState.COMPLETE, repeatCount: 1 });
     });
 
-    it('should run (repeats + 1) * spool.length times', () => {
-      const runDelay = preTypeDelay + typeDelay + preEraseDelay + eraseDelay;
-      const instance = renderWithOptions(1, true, ['a', 'b']);
+    it('should run (repeats + 1) * spool.length times', async () => {
+      const instance = renderWithOptions(1, false, ['a', 'b']);
       const expectState = testInstance(instance);
 
-      jest.advanceTimersByTime(runDelay);
+      await advanceTimersByTime(preTypeDelay, preEraseDelay);
+      expectState({ spoolIndex: 0, repeatCount: 0 });
+      await advanceTimersByTime(preTypeDelay, preEraseDelay);
       expectState({ spoolIndex: 1, repeatCount: 0 });
-      jest.advanceTimersByTime(runDelay);
-      expectState({ spoolIndex: 0, repeatCount: 1 });
 
-      jest.advanceTimersByTime(runDelay);
-      expectState({ spoolIndex: 1, repeatCount: 1 });
-      jest.advanceTimersByTime(runDelay);
+      await advanceTimersByTime(preTypeDelay, preEraseDelay);
+      expectState({ spoolIndex: 0, repeatCount: 1 });
+      await advanceTimersByTime(preTypeDelay, preEraseDelay);
       expectState({ spoolIndex: 1, repeatCount: 1, typerState: TyperState.COMPLETE });
+    });
+  });
+
+  describe('event handlers', () => {
+    const renderWithCallback = (type: string, handler: () => void, spool: string[], repeats?: number) => (
+      renderWithProps({
+        spool,
+        preTypeDelay,
+        typeDelay,
+        preEraseDelay,
+        eraseDelay,
+        repeats,
+        [type]: handler,
+      })
+    );
+
+    it('should call onType', async () => {
+      const onType = jest.fn();
+      renderWithCallback('onType', onType, ['ab']);
+
+      await advanceTimersByTime(preTypeDelay);
+      expect(onType.mock.calls.length).toBe(1);
+      expect(onType.mock.calls[0][0]).toBe('a');
+
+      await advanceTimersByTime(typeDelay);
+      expect(onType.mock.calls.length).toBe(2);
+      expect(onType.mock.calls[1][0]).toBe('ab');
+    });
+
+    it('should call onTyped', async () => {
+      const onTyped = jest.fn();
+      renderWithCallback('onTyped', onTyped, ['ab']);
+
+      await advanceTimersByTime(preTypeDelay);
+      expect(onTyped.mock.calls.length).toBe(0);
+
+      await advanceTimersByTime(typeDelay);
+      expect(onTyped.mock.calls.length).toBe(1);
+      expect(onTyped.mock.calls[0][0]).toBe('ab');
+    });
+
+    it('should call onErase', async () => {
+      const onErase = jest.fn();
+      renderWithCallback('onErase', onErase, ['ab']);
+
+      await advanceTimersByTime(preTypeDelay, typeDelay, preEraseDelay);
+      expect(onErase.mock.calls.length).toBe(1);
+      expect(onErase.mock.calls[0][0]).toBe('a');
+
+      await advanceTimersByTime(eraseDelay);
+      expect(onErase.mock.calls.length).toBe(2);
+      expect(onErase.mock.calls[1][0]).toBe('');
+    });
+
+    it('should call onErased', async () => {
+      const onErased = jest.fn();
+      const i = renderWithCallback('onErased', onErased, ['ab']);
+
+      await advanceTimersByTime(preTypeDelay, typeDelay, preEraseDelay);
+
+      expect(onErased.mock.calls.length).toBe(0);
+
+      await advanceTimersByTime(eraseDelay);
+      expect(onErased.mock.calls.length).toBe(1);
+      expect(onErased.mock.calls[0][0]).toBe('ab');
+    });
+
+    it('should call onFinish', async () => {
+      const onFinish = jest.fn();
+      renderWithCallback('onFinish', onFinish, ['a'], 0);
+
+      await advanceTimersByTime(preTypeDelay);
+      expect(onFinish.mock.calls.length).toBe(1);
     });
   });
 });
--- tests/unit/utils.ts
+++ tests/unit/utils.ts
@@ -4,7 +4,7 @@ import { ShallowWrapper } from 'enzyme';
 export function testInstance<C extends Component>(instance: ShallowWrapper<C['props'], C['state'], C>) {
   return function expectState(expectMap: Partial<C['state']>) {
     Object.keys(expectMap).forEach((key) => {
-      expect(expectMap[key as keyof C['state']]).toBe(instance.state(key));
+      expect(instance.state(key)).toBe(expectMap[key as keyof C['state']]);
     });
   };
 }

a91cac6937721dbec0803efc92fbdc8e528635bf
Andrew Li <andrewli357@gmail.com>
2019-03-21 12:22:46 -0500

    Add Typer demo

---
+++ demo/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Typer from 'react-typer';
+
+class App extends Component {
+  onType = (typed) => {
+    console.log('%conType:   ', 'color:red', typed);
+  }
+
+  onTyped = (text) => {
+    console.log('%conTyped:  ', 'color:blue', text);
+  }
+
+  onErase = (erased) => {
+    console.log('%conErase:  ', 'color:green', erased);
+  }
+
+  onErased = (text) => {
+    console.log('%conErased: ', 'color:orange', text);
+  }
+
+  onFinish = () => {
+    console.log('Finished!');
+  }
+
+  render() {
+    const spool = ['🎉 React Typer', '🎊 Hello World!'];
+
+    return (
+      <div className="App">
+        <Typer
+          repeats={1}
+          initialAction="erasing"
+          spool={spool}
+          onType={this.onType}
+          onTyped={this.onTyped}
+          onErase={this.onErase}
+          onErased={this.onErased}
+          onFinish={this.onFinish}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;

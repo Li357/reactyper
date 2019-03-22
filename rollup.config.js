c97595060945885118bb578e0bf07f39c787d5da
Andrew Li <andrewli357@gmail.com>
2019-03-21 20:42:23 -0500

    Update rollup builds, bundle lodash.split for browser

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import autoprefixer from 'autoprefixer';
 import { terser } from 'rollup-plugin-terser';
@@ -11,34 +12,15 @@ const getDependencies = ({ dependencies = {}, peerDependencies = {} }) => [
   ...Object.keys(peerDependencies),
 ];
 
-export default {
+const base = {
   input: 'src/index.tsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'esm',
-    },
-    {
-      file: 'dist/react-typer.min.js',
-      format: 'umd',
-      globals: {
-        react: 'React',
-        'lodash.split': '_.split',
-      },
-      name: 'Typer',
-    }
-  ],
-  external: getDependencies(pkg),
   plugins: [
     typescript({
       typescript: require('typescript'),
       objectHashIgnoreUnknownHack: true,
     }),
     resolve(),
+    commonjs(),
     postcss({
       plugins: [
         autoprefixer({ browsers: ['last 2 versions'] }),
@@ -51,3 +33,35 @@ export default {
     }),
   ],
 };
+
+export default [
+  {
+    ...base,
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    external: getDependencies(pkg),
+  },
+  {
+    ...base,
+    output: {
+      file: pkg.module,
+      format: 'esm',
+    },
+    external: getDependencies(pkg),
+  },
+  {
+    ...base,
+    output: {
+      file: 'dist/react-typer.min.js',
+      format: 'umd',
+      globals: {
+        react: 'React',
+        'lodash.split': '_.split',
+      },
+      name: 'Typer',
+    },
+    external: Object.keys(pkg.peerDependencies),
+  },
+];
